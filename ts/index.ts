18a3502756649d2d62ff939a73bb41404d3fded1
Matt Deeds <mdeeds@gmail.com>
2021-06-06 20:44:38 -0700

    Add memory counter.

--- ts/index.ts
+++ ts/index.ts
@@ -4,6 +4,16 @@ import { LoopManager } from "./loopManager";
 import { SampleSource } from "./sampleSource";
 
 async function go() {
+  const body = document.getElementsByTagName('body')[0];
+  const mm = document.createElement('div');
+  const report = function () {
+    const mem: any = window.performance['memory'];
+    mm.innerText = `${(mem.usedJSHeapSize / 1000000).toFixed(3)}MB`;
+    setTimeout(report, 100);
+  }
+  body.appendChild(mm);
+  report();
+
   const a = await Audio.make();
   const s = await SampleSource.make(a);
   let l = new Loop(s);
@@ -12,7 +22,6 @@ async function go() {
 
   const lm = new LoopManager(a, l);
 
-  const body = document.getElementsByTagName('body')[0];
   let changeRate = 0;
   body.addEventListener('keydown', (ev: KeyboardEvent) => {
     switch (ev.code) {
